a9a0458494c9f17601a9c1533f003b6dd8caea2d
furat95 <furat95@live.nl>
2017-12-13 23:54:15 +0100

    Stardate Model files zitten hierin

---
+++ Minder/Models/Enums.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Minder.Models
+{
+    public enum GenderEnum
+    {
+        Male,
+        Female
+    }
+
+    public enum GenderInterestEnum
+    {
+        Women,
+        Men,
+        Both
+    }
+
+    public enum EthnicityEnum
+    {
+        European,
+        Asian,
+        African,
+        Latino
+    }
+
+    public class EthnicityEnumSelection
+    {
+        public EthnicityEnum Ethnicity { get; set; }
+        public bool IsSelected { get; set; }
+    }
+
+    public enum EducationEnum
+    {
+        None,
+        [Display(Name = "High school")]
+        Highschool,
+        University
+    }
+
+    public class EducationEnumSelection
+    {
+        public EducationEnum Education { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
